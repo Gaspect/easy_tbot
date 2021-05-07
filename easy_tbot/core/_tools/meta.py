cf24fb4e6000b8e3a7fbb69fae37d1c14ad0e9e1
Jesús Enrique <jesusefg12@gmail.com>
2021-05-07 17:08:48 -0400

    added main package doc, fixed meta.py

--- easy_tbot/core/_tools/meta.py
+++ easy_tbot/core/_tools/meta.py
@@ -1,32 +1,47 @@
 import types
 import sys
+
+# This requires a bit of explanation: The purpose is create a class that can make a dummy
+# metaclass for one level of class instantiation that replaces itself with
+# the actual metaclass.
+
 class MultiMetaFactory:
+    '''
+    Factory class that create dummy metaclasses blending multiples metclasses and normal classes.
+    '''
     
     def __getitem__(self, base_class):
         """Create a base class with a set of many bases classes, simultaneous metaclass are allowed."""
-        # This requires a bit of explanation: the basic idea is to make a dummy
-        # metaclass for one level of class instantiation that replaces itself with
-        # the actual metaclass.
+       
+        #We declare a variable for store meta classes
         metas = []
+
+        #and another to store normal base classes
         bases = []
         
+        #and now we split them 
         for _class in base_class:
             if issubclass(_class, type):
                 metas.append(_class)
             else:
                 bases.append(_class)
 
+        # turn the bases list into a tuple
         bases = tuple(bases)
+
+        # and create a base class that inherits from all meta classes filtered
         class meta_mix(*metas):
             pass
-
+        
 
         class metaclass(type):
 
             def __new__(cls, name, this_bases, d):
                 if sys.version_info[:2] >= (3, 7):
+
                     # This version introduced PEP 560 that requires a bit
                     # of extra care (we mimic what is done by __build_class__).
+                    
                     resolved_bases = types.resolve_bases(bases)
                     if resolved_bases is not bases:
                         d['__orig_bases__'] = bases
@@ -40,4 +55,5 @@ class MultiMetaFactory:
 
         return type.__new__(metaclass, 'temporary_class', (), {})
 
+# Now we create a instance for easing developers life.
 MultiMeta = MultiMetaFactory()
